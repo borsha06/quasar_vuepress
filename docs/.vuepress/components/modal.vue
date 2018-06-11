3cdaaed4a2b888b851ed28463b4c5ccb87384ad4
borsha06 <tasfia.sharmin1994@gmail.com>
2018-06-11 23:57:23 +0600

    message

---
+++ docs/.vuepress/components/modal.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="modal">
+        <q-btn @click="modal">Do something</q-btn>
+    </div>
+
+</template>
+<script>
+    import Vue from 'vue';
+    import Quasar from "quasar-framework/dist/quasar.mat.esm";
+    import "quasar-framework/dist/umd/quasar.mat.css";
+    import Swal from 'sweetalert2';
+    import { QBtn } from "quasar-framework/dist/quasar.mat.esm";
+
+    Vue.use(Quasar);
+
+    export default{
+        name: "App",
+        components: {
+            QBtn,
+        },
+
+        data(){
+            return {
+                opened: false
+            }
+        },
+        methods: {
+
+
+            modal: function () {
+                Swal('Hello world!')
+            }
+        }
+    }
+
+</script>
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+# Hello VuePress
+# vuepress-start
+
+<modal></modal>
+## Local Development
+
+
+For more information, see [VuePress Docs](https://vuepress.vuejs.org)
